0d64a091e165849f1a93a336b99c43b7f6a40185
AndrewKL <andrew.long.3001@gmail.com>
2014-07-05 11:58:21 -0500

    initial commit

---
+++ OffHeapStorage.tests/OffHeapStorageTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProtoBuf;
+
+namespace OffHeapStorage.tests
+{
+    [TestFixture]
+    public class OffHeapStorageTests
+    {
+        [Serializable]
+        [ProtoContract]
+        public class TestClass
+        {
+            [ProtoMember(1)]
+            public int A {get;set;}
+            [ProtoMember(2)]
+            public string B {get;set;}
+        }
+        public IEnumerable<TestClass> GetIEnumerableOfTestClass(int size)
+        {
+            for(int i = 0;i<size;i++){
+                yield return new TestClass()
+                {
+                    A = i,
+                    B = i.ToString()
+                };
+            }
+        }
+
+        [TestCase(10)]
+        [TestCase(100)]
+        [TestCase(1000)]
+        [TestCase(10000)]
+        [TestCase(100000)]
+        [TestCase(1000000)]
+        public void CanCreateOffHeapStorage(int sizeOfIEnumerable)
+        {
+            var storage = new OffHeapIEnumerable<TestClass>(GetIEnumerableOfTestClass(sizeOfIEnumerable));
+
+            Assert.AreEqual(sizeOfIEnumerable, storage.ToList().Count);
+        }
+    }
+}
---
+++ OffHeapStorage/OffHeapStorage.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection;
+using ProtoBuf;
+using ProtoBuf.Meta;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OffHeapStorage
+{
+    public class OffHeapIEnumerable<T> : IEnumerable<T>
+    {
+        private readonly MemoryStream _stream;
+        //private BinaryFormatter _binaryFormatter;
+
+        public OffHeapIEnumerable(IEnumerable<T> input)
+        {
+            _stream = new MemoryStream();
+
+            //var spec = RuntimeTypeModel.Default.Add(typeof(T), true);
+            //var props = typeof(T).GetTypeInfo().DeclaredProperties.Where(x =>
+            //    x.PropertyType == typeof (int)
+            //    || x.PropertyType == typeof (string)
+            //    || x.PropertyType == typeof (decimal)).ToList();
+
+            //for(var i = 0;i<props.Count;i++ )
+            //{
+            //    spec.Add(i+1, props[i].Name);
+            //}
+            Serializer.Serialize<IEnumerable<T>>(_stream, input);
+        }       
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            _stream.Position = 0;
+            return Serializer.Deserialize<IEnumerator<T>>(_stream);
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
